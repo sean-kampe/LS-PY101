800ed683652213f50db3ed7172223d8928f99ab7
sean-kampe <plrtz.glrb.4@gmail.com>
2024-11-14 23:03:00 -0800

    add medium.py

---
+++ lesson_3/medium.py
@@ -0,0 +1,25 @@
+message = '-The Flintstones Rock!'
+for _ in range(10):
+    print(message)
+    message = '-' + message
+# LS solution
+for padding in range(1, 11):
+    print(f'{"-" * padding}The Flintstones Rock!')
+
+def factors(number):
+    divisor = number
+    result = []
+    while divisor > 0:
+        if number % divisor == 0:
+            result.append(number // divisor)
+        divisor -= 1
+    if result == []:
+        print('Please enter a positive integer and try again')
+    else:
+        return result
+
+import math
+
+nan = float('nan')
+print(math.isnan(nan))
+
