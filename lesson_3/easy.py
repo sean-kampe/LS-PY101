431462ced8f565efadbb1f40d9f234ff52e816ca
sean-kampe <plrtz.glrb.4@gmail.com>
2024-11-14 20:37:13 -0800

    More problems completed

--- lesson_3/easy.py
+++ lesson_3/easy.py
@@ -55,3 +55,69 @@ numbers = [1, 2, 3, 4, 5, 15, 16, 17, 95, 96, 99]
 
 number1 = 8  # False (not in numbers)
 number2 = 95 # True (in numbers)
+print(number1 in numbers)
+print(number2 in numbers)
+
+value = 42
+for _ in range(10, 101):
+    if _ == value:
+        print('YES')
+        break
+    elif _ == 100:
+        print('no')
+
+second_value = 101
+for _ in range(10, 101):
+    if _ == second_value:
+        print('YES')
+        break
+    elif _ == 100:
+        print('no')
+
+# Above code works, but this is so much better
+print(42 in range(10, 101))
+
+more_numbers = [1, 2, 3, 4, 5]
+del more_numbers[2]
+print(more_numbers)
+
+table = {'field1': 1, 'field2': 2, 'field3': 3, 'field4': 4}
+print(isinstance(more_numbers, list))
+print(isinstance(table, list))
+
+title = "Flintstone Family Members"
+centered_title = title.center(40)
+
+statement1 = "The Flintstones Rock!"
+statement2 = "Easy come, easy go."
+print(statement1.count('t'))
+print(statement2.count('t'))
+
+ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 402, 'Eddie': 10}
+print('Spot' in ages)
+additional_ages = {'Marilyn': 22, 'Spot': 237}
+ages.update(additional_ages)
+print(ages)
+
+nums = [1, 2, 3, 4]
+nums.clear()
+del nums[:]
+
+print([1, 2, 3] + [4, 5])
+
+string1 = "hello there"
+string2 = str1
+string2 = "goodbye!"
+print(string2)
+
+my_list1 = [{"first": "value1"}, {"second": "value2"}, 3, 4, 5]
+my_list2 = my_list1.copy()
+my_list2[0]['first'] = 42
+print(my_list1)
+
+def is_color_valid(color):
+    return color == "blue" or color == "green"
+def is_color_valid_2(color):
+    colors = ['blue', 'green']
+    return color in colors
+print(is_color_valid_2('green'))
